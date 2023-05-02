eb942e63c6db3819814d7e8bcf7bf4d4a300f80d
joseph H <hendryj0@protonmail.com>
2023-05-03 11:46:33 +1200

    Add random coach function

--- src/main/Coach.java
+++ src/main/Coach.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Random;
+
 /**
  * This class implements a Coach.
  * This stores a name, rarity, value and stats.
@@ -18,6 +20,63 @@ public class Coach extends Person {
         super(name, rarity, value);
         this.stats = stats;
     }
+
+    /**
+	 * Creates and returns a random coach with the given rarity.
+	 * @param rarity The rarity of the Coach.
+	 * @return The random Coach with the given rarity.
+	 */
+	public static Coach createRandomCoach(String rarity) {
+
+		Random random = new Random();
+
+		// Generate the name of the coach (Coach + random number)
+		String name = "Coach" + random.nextInt(1000);
+
+		// Generate the value and stats of the coach based on the rarity
+		int value = 6;
+		int[] stats = new int[3];
+
+		if (rarity == "Bronze") {
+			// If the rarity is bronze the rating is between 40 - 60 and the value is between 400 - 600
+			value = random.nextInt(200) + 400;
+			stats[0] = random.nextInt(20) + 40;
+			stats[1] = random.nextInt(20) + 40;
+			stats[2] = random.nextInt(20) + 40;
+
+		} else if (rarity == "Silver") {
+			// If the rarity is silver the rating is between 60 - 80 and the value is between 600 - 800
+			value = random.nextInt(200) + 600;
+			stats[0] = random.nextInt(20) + 60;
+			stats[1] = random.nextInt(20) + 60;
+			stats[2] = random.nextInt(20) + 60;
+
+		} else if (rarity == "Gold") {
+			// If the rarity is gold the rating is between 80 - 90 and the value is between 800 - 900
+			value = random.nextInt(100) + 800;
+			stats[0] = random.nextInt(10) + 80;
+			stats[1] = random.nextInt(10) + 80;
+			stats[2] = random.nextInt(10) + 80;
+
+		} else {
+			// If the rarity is platinum the rating is between 90 - 100 and the value is between 900 - 1000
+			value = random.nextInt(100) + 900;
+			stats[0] = random.nextInt(10) + 90;
+			stats[1] = random.nextInt(10) + 90;
+			stats[2] = random.nextInt(10) + 90;
+		}
+
+		// Return the new coach
+		return new Coach(name, rarity, value, stats);
+	}
+
+    /**
+	 * Returns values of the Coach in a String format.
+	 * @return Coach values in string format.
+	 */
+	public String toString() {
+		return "Coach [name=" +super.name+ ", rarity=" +super.rarity+ ", value=" +super.value+ ", stats=[" +stats[0]+ ", " +stats[1]+ ", " +stats[2]+ "]";
+	}
 
     /**
      * This method is used to geth the stats of the Coach.
