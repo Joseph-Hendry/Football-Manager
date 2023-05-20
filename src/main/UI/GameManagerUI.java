92d4f0746ab585fb8bebd8dbd71485bf4eb8c099
joseph H <hendryj0@protonmail.com>
2023-05-21 11:35:01 +1200

    GameManager interface.

--- src/main/UI/GameManagerUI.java
+++ src/main/UI/GameManagerUI.java
@@ -1,11 +1,18 @@
 package main.UI;
+
 import main.body.GameManager;
 import main.body.Match;
 
+
+/**
+ * The interface for the {@link GameManager} UI.
+ */
 public interface GameManagerUI {
 	
 	/**
 	 * Initializes the UI and allows the user to set up their team.
+	 * 
+	 * @param manager The game {@link GameManager}.
 	 */
 	public void setup(GameManager manager);
 
@@ -35,18 +42,27 @@ public interface GameManagerUI {
 	public void playMatch(Match match);
 	
 	/**
-	 * Shows the market where the player can buy and sell players, coaches and items.
+	 * Shows the market where the player can buy players, coaches and items.
 	 */
 	public void storeMenu();
 
 	/**
-	 * Shows message.
+	 * Shows message to the user.
 	 */
 	public void showMessage(String message);
 
+	/**
+	 * Shows the end game screen.
+	 */
     public void endGame();
 
+	/**
+	 * Confirms if the user wants to quit the game.
+	 */
 	public boolean confirmQuit();
 
+	/**
+	 * Quits the game.
+	 */
 	public void quit();
 }
